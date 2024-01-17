d2c32a3de450c327b384f2d72fcd0438678d41fb
Abeera Fatima <120410882+melodyBee@users.noreply.github.com>
2024-01-17 15:36:52 +0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+a=int(input("num 1= "))
+b=int(input("num 2= "))
+c=input("Enter any sign +,-,*,/ = ")
+if c=="+":
+    print(a+b)
+elif c=="-":
+    print(a-b)
+elif c=="*":
+    print(a*b)
+elif c=="/":
+    print(a/b)
+else:
+    print("You have entered wrong sign.")
+    
