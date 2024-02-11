35f8b8e06a0ce358ec19d49783ab383b684c46d3
Abeera Fatima <abeerafatimarub@gmail.com>
2024-02-11 17:33:06 +0500

    Refactor calculator code to include a loop and improve user experience

--- Calculator.py
+++ Calculator.py
@@ -1,14 +1,24 @@
-a=int(input("num 1= "))
-b=int(input("num 2= "))
-c=input("Enter any sign +,-,*,/ = ")
-if c=="+":
-    print(a+b)
-elif c=="-":
-    print(a-b)
-elif c=="*":
-    print(a*b)
-elif c=="/":
-    print(a/b)
-else:
-    print("You have entered wrong sign.")
-    
+while True:
+    a = int(input("num 1 = "))
+    b = int(input("num 2 = "))
+    c = input("Enter any sign +, -, *, / = ")
+
+    if c == "+":
+        result = a + b
+    elif c == "-":
+        result = a - b
+    elif c == "*":
+        result = a * b
+    elif c == "/":
+        result = a / b
+    else:
+        result = "You have entered the wrong sign."
+
+    print(result)
+    print("Do you want to continue? (yes/no)")
+    cont = input()
+    if cont == "no":
+        break
+    else:
+        continue
+
